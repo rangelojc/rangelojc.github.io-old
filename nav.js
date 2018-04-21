176622d2136fed75bbccf83c3c871deb2f134afb
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2018-04-21 20:23:23 +0800

    asd
    asd

--- nav.js
+++ nav.js
@@ -5,12 +5,8 @@ function openURL(url) {
 
 function hoverSocial(el, action) {
 	switch (action) {
-		case true:
-			el.style.opacity = 1;
-			break;
-		case false:
-			el.style.opacity = 0.7;
-			break;
+		case true: el.style.opacity = 1; break;
+		case false: el.style.opacity = 0.8; break;
 	}
 }
 
