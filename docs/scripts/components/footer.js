6c2c1895b9b404d1a0aa5f2f4daf0ce00f204c0d
Angelo <ramuzuconcepcion@gmail.com>
2022-03-22 14:02:40 +0800

    Build docs folder

---
+++ docs/scripts/components/footer.js
@@ -0,0 +1,29 @@
+const Footer = function () {
+    const footer = {};
+
+    footer.setDate = function () {
+        copyright_date.innerHTML = "Ramce Concepcion &copy; " + new Date().getFullYear();
+    }
+
+    footer.openURL = function (url) {
+        const win = window.open(url, '_blank');
+        win.focus();
+    }
+
+    footer.setEvents = function () {
+        sfb.onclick = function () { footer.openURL('https://www.facebook.com/ramceconcepcion'); };
+        sig.onclick = function () { footer.openURL('https://www.instagram.com/rangelojc/'); };
+        sgl.onclick = function () { footer.openURL('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=ramceconcepcion@gmail.com'); };
+        sgh.onclick = function () { footer.openURL('https://github.com/rangelojc'); };
+        sli.onclick = function () { footer.openURL('https://linkedin.com/in/ramceconcepcion'); };
+    }
+
+    footer.init = function () {
+        footer.setEvents();
+        footer.setDate();
+    }
+
+    return footer;
+}();
+
+export default Footer;
